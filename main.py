02f2ce6a68d484a5dfe1e5c33cd7446b3ff65c42
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-03-04 18:51:30 +0000

    #1 graph creation

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import requests
 
+USERNAME = "krall"
+TOKEN = "asdf54dsafg64sdf"
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
+GRAPH_ENDPOINT = F"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
 
 
 class HabitTracker:
     def __init__(self):
         user_params = {
-            "token": "asdf54dsafg64sdf",
-            "username": "krall",
+            "token": TOKEN,
+            "username": USERNAME,
             "agreeTermsOfService": "yes",
             "notMinor": "yes",
         }
-        response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-        print(response.text)
+        # user creation not required anymore as already sent and now only returning a "user already exists"
+        # requests.post(url=PIXELA_ENDPOINT, json=user_params)
+
+        graph_config = {
+            "id": "graph1",
+            "name": "Cycling Graph",
+            "unit": "Km",
+            "type": "float",
+            "color": "ajisai"
+        }
+        headers = {
+            "X-USER-TOKEN": TOKEN
+        }
+
+        # Graph already created and visible at https://pixe.la/v1/users/krall/graphs/graph1.html
+        # requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
 
 
 if __name__ == '__main__':
